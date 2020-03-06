1ff4a9fdb24772cfdca090ead8a4d458f2031e4c
RDR3 <48764809+RDR3@users.noreply.github.com>
2020-03-06 05:48:13 -0800

    Add files via upload

---
+++ testUpdated2.java
@@ -0,0 +1,7 @@
+public class test{
+	public static void main(String[] args) throws IOException {
+		System.out.println("This is a Java file");
+		String password = "password123";
+		String username = "John";
+	};
+}
