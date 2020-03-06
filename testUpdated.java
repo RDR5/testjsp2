6e3d4eeec77077031aabacdbcf8c37db50ede663
RDR3 <48764809+RDR3@users.noreply.github.com>
2020-03-06 06:17:06 -0800

    Delete testUpdated.java

--- testUpdated.java
+++
@@ -1,7 +0,0 @@
-public class test{
-	public static void main(String[] args) throws IOException {
-		System.out.println("This is a Java file");
-		String password = "password123";
-		String username = "John";
-	};
-}
